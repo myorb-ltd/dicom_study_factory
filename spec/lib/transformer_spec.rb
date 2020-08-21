b203d741c21d57e7b6d4acdcd40f629d8dc469ee
Diego Lagos <diego@myorb.com>
2020-08-21 12:57:35 +0100

    Added .dcm file to test

--- spec/lib/transformer_spec.rb
+++ spec/lib/transformer_spec.rb
@@ -1,21 +1,41 @@
 # frozen_string_literal: true
 
 describe DicomStudyFactory::Transformer do
+  let(:source_dir) { 'tmp/source' }
+  let(:output_dir) { 'tmp/output' }
   describe 'default directories' do
     before { described_class.new }
-    let(:source_dir) { 'tmp/source' }
-    let(:output_dir) { 'tmp/output' }
     it { expect(described_class::SOURCE_DIR).to eq source_dir }
     it { expect(described_class::OUTPUT_DIR).to eq output_dir }
     it { expect(Dir.exist?(source_dir)).to eq true }
     it { expect(Dir.exist?(output_dir)).to eq true }
   end
+end
+
+describe DicomStudyFactory::Transformer do
   describe '#files' do
-    let(:files) { subject.files }
-    let(:output_files) { subject.output_files }
+    let(:source_dir) { 'tmp/source' }
+    after { FileUtils.rm_r source_dir }
     context 'by default are empty' do
-      it { expect(files).to be_empty }
-      it { expect(output_files).to be_empty }
+      it { expect(subject.files).to be_empty }
+      it { expect(subject.output_files).to be_empty }
+    end
+    context 'with source files' do
+      describe '.dcm files' do
+        let(:zip) { 'spec/support/FluoroWithDisplayShutter.dcm.zip' }
+        let(:zip_url) { 'http://www.dclunie.com/images/FluoroWithDisplayShutter.dcm.zip' }
+        let(:support_dir) { 'spec/support/dicoms' }
+        let(:download_zip) do
+          system("curl #{zip_url} --output #{zip}")
+          FileUtils.mkdir_p support_dir
+          system("unzip #{zip} -d #{support_dir}")
+        end
+        before do
+          download_zip unless File.exist?(zip)
+          FileUtils.cp_r support_dir, described_class::SOURCE_DIR
+        end
+        it { expect(subject.files).not_to be_empty }
+      end
     end
   end
 end
